e9a11438f29a779a0b545539f3b45d2793ede8f7
sandyrod <sandyrod@gmail.com>
2025-11-19 21:20:03 -0400

    municipios y parroquias

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstadoController;
+use App\Http\Controllers\MunicipioController;
+use App\Http\Controllers\ParroquiaController;
+
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::apiResource('estados', EstadoController::class);
 Route::apiResource('municipios', MunicipioController::class);
+Route::apiResource('parroquias', ParroquiaController::class);
